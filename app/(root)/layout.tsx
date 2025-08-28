98713532b3595c55fb4da0871e068af4a1004a54
Azad Hüseyin Kılıç <azadhuseyinkilic@Azads-MacBook-Pro.local>
2025-08-28 16:03:42 +0300

    initial commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+import Navbar from "../components/Navbar";
+
+
+type LayoutProps = {
+    children: React.ReactNode;
+  };
+  
+  export default function Layout({ children }: LayoutProps) {
+    return <main className="font-work-sans">
+        <Navbar />
+        {children}
+        </main>;
+  }
+  
